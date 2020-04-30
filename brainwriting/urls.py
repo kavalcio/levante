a7c20c3c0583240900e0ed6a8d12d607da7b63a8
Peter <peterathanas@hotmail.com>
2020-04-30 00:45:47 -0400

    Implemented nomination page and updated back end functions
    - Made the nomination page
    - Implemented upd_response and utilized it to update the nominations field of response objects
    - Updated get_response to only send ideas that are not nominated and to send an error code if all ideas are nominated

--- brainwriting/urls.py
+++ brainwriting/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('add_response/', views.add_response, name='add_response'),
     path('get_response/', views.get_response, name='get_response'),
     path('add_question/', views.add_question, name='add_question'),
+    path('upd_response/', views.upd_response, name='upd_response'),
     path('<str:room_name>/<str:user_name>/', views.room, name='room'),
     path('<str:room_name>/<str:user_name>/inspiration/', views.inspiration, name='inspiration'),
+    path('<str:room_name>/<str:user_name>/nominate/', views.nominate, name='room'),
 ]
--- brainwriting/views.py
+++ brainwriting/views.py
@@ -33,6 +33,17 @@ def add_question(request):
 
     return HttpResponse(request.session)
 
+def upd_response(request):
+    if not request.is_ajax() or not request.method=='POST':
+        return HttpResponseNotAllowed(['POST'])
+    nomination = request.POST['nomination']
+    responseToUp = request.POST['response']
+    if(nomination == "yes"):
+       temp = Response.objects.get(response_text=responseToUp)
+       temp.nominations = 1 
+       temp.save()
+    return HttpResponse(request.session)
+
 def add_response(request):
     print(request)
     if not request.is_ajax() or not request.method=='POST':
@@ -51,6 +62,18 @@ def add_response(request):
 
     return HttpResponse(request.session)
 
+#only sends back an idea that has not been nominated yet
 def get_response(request):
-    response = Response.objects.order_by('?').first()
-    return HttpResponse(response)
+    response = Response.objects.filter(nominations = 0)
+    if(Response.objects.all().count()==0):
+        return HttpResponse("#-----objects not found-------#")
+    #sends code if all ideas have been nominated
+    if(not response):
+        return HttpResponse("NULL869")
+    return HttpResponse(response.order_by('?').first())
+
+def nominate(request, room_name, user_name):
+    return render(request, 'brainwriting/nominate.html', {
+        'room_name': room_name,
+        'user_name': user_name
+    })
